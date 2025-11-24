1e73a94111ed8c99a015d8873d9a96bf24fdeecd
Ranjeet Rathour <ranjeet.rathour@xcelore.com>
2025-11-24 11:26:09 +0530

    EvenOddPredicitionWithoutIf

--- src/main/java/org/example/logic/EvenOddPredicitionWithoutIf.java
+++ src/main/java/org/example/logic/EvenOddPredicitionWithoutIf.java
@@ -1,4 +1,12 @@
 package org.example.logic;
 
 public class EvenOddPredicitionWithoutIf {
+    public static void main(String[] args) {
+        System.out.println(isEvenOrOdd(8));
+    }
+
+    static String isEvenOrOdd(int n){
+        String[] checker = {"Even","Odd"};
+        return checker[n%2];
+    }
 }
