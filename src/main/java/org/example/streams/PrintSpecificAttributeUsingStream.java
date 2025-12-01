7ce5cfe06e7aba886fdf7e6c1e3b623b41db76e3
Ranjeet Rathour <ranjeet.rathour@xcelore.com>
2025-12-01 12:18:24 +0530

    string

--- src/main/java/org/example/streams/PrintSpecificAttributeUsingStream.java
+++ src/main/java/org/example/streams/PrintSpecificAttributeUsingStream.java
@@ -1,4 +1,33 @@
 package org.example.streams;
 
-public class PrintSpecififcAttributeUsiingMap {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintSpecificAttributeUsingStream {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee("Amit Kumar", "HR", 55000, LocalDateTime.now().minusYears(10)));
+        employees.add(new Employee("Rohit Sharma", "IT", 75000, LocalDateTime.now().minusDays(5)));
+        employees.add(new Employee("Neha Verma", "Finance", 68000, LocalDateTime.now().minusYears(3)));
+        employees.add(new Employee("Suresh Singh", "Sales", 52000, LocalDateTime.now().minusYears(20)));
+        employees.add(new Employee("Priya Gupta", "Marketing", 60000, LocalDateTime.now().minusDays(7)));
+        employees.add(new Employee("Arjun Yadav", "IT", 80000, LocalDateTime.now().minusYears(15)));
+        employees.add(new Employee("Karan Malhotra", "HR", 48000, LocalDateTime.now().minusDays(1)));
+        employees.add(new Employee("Deepak Joshi", "Finance", 72000, LocalDateTime.now().minusYears(12)));
+        employees.add(new Employee("Sneha Kulkarni", "IT", 65000, LocalDateTime.now().minusDays(8)));
+        employees.add(new Employee("Rahul Tiwari", "IT", 40000, LocalDateTime.now().minusDays(18)));
+        employees.add(new Employee("Pooja Sharma", "Marketing", 53000, LocalDateTime.now().minusDays(9)));
+        employees.add(new Employee("Aditya Kapoor", "Finance", 78000, LocalDateTime.now().minusDays(4)));
+        employees.add(new Employee("Sunil Deshmukh", "HR", 50000, LocalDateTime.now().minusDays(11)));
+        employees.add(new Employee("Harsh Mehta", "IT", 90000, LocalDateTime.now().minusDays(6)));
+        employees.add(new Employee("Komal Jain", "Sales", 47000, LocalDateTime.now().minusDays(2)));
+
+        employees.stream().filter(employee -> employee.getDept().equalsIgnoreCase("it") && employee.getSalary()>50000)
+                .forEach(e->{
+                    System.out.println(e.getName()+" "+e.getDept()+" "+e.getSalary());
+                });
+
+    }
 }
--- src/main/java/org/example/string/ReverseWordsByJava8.java
+++ src/main/java/org/example/string/ReverseWordsByJava8.java
@@ -1,4 +1,16 @@
 package org.example.string;
 
-public class ReverseStringByJava8 {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ReverseWordsByJava8 {
+
+    public static void main(String[] args) {
+        String string = "hello my name is ranjeet";
+        String[] s = string.split(" ");
+        String reverseWords = Arrays.stream(s).map(s1 -> new StringBuilder(s1).reverse()).collect(Collectors.joining(" "));
+        System.out.println(reverseWords);
+
+    }
+
 }
