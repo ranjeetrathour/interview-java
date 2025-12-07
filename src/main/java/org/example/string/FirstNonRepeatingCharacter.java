317283f53269df184fca517fa68b8f233a32a0f3
Ranjeet Rathour <ranjeet.rathour@xcelore.com>
2025-12-07 18:50:54 +0530

    first non repating without stream

--- src/main/java/org/example/string/FirstNonRepeatingCharacter.java
+++ src/main/java/org/example/string/FirstNonRepeatingCharacter.java
@@ -1,4 +1,20 @@
 package org.example.string;
 
 public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String s = "aabccdeef";
+        int charFreq[] = new int[26]; //initial all 26 is 0
+        for (int i=0; i<s.length();i++){
+            charFreq[s.charAt(i)-'a']++;
+        }
+
+        for (int i=0; i<s.length();i++){
+            if(charFreq[s.charAt(i)-'a']==1){
+                System.out.println(s.charAt(i));
+                break;
+
+            }
+        }
+
+    }
 }
