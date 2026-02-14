b81c3dafab02d76b97fd8eb294783e7bc0de963c
ranjeetrathour <ranjeetrathour@example.com>
2026-02-14 13:57:12 +0000

    sh

---
+++ src/main/java/org/example/arrays/MoveZerosWithOrdered.java
@@ -0,0 +1,22 @@
+package org.example.arrays;
+
+import java.util.Arrays;
+
+/**
+ * leetcode 283
+ */
+public class MoveZerosWithOrdered {
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        int count=0;
+        for (int i = 0; i < nums.length ; i++) {
+            if(nums[i]!=0){
+                nums[count++]=nums[i];
+            }
+        }
+        while (count<nums.length){
+            nums[count++]=0;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
