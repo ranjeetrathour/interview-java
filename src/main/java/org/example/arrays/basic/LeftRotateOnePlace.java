55efae096a33d8d012b62dbe9b34ccaa0b06c53e
ranjeetrathour <ranjeetrathour@example.com>
2026-04-07 12:21:32 +0100

    left rotate by brut force

--- src/main/java/org/example/arrays/basic/LeftRotateOnePlace.java
+++ src/main/java/org/example/arrays/basic/LeftRotateOnePlace.java
@@ -2,16 +2,28 @@ package org.example.arrays.basic;
 
 import java.util.Arrays;
 
+/**
+ *
+ */
 public class LeftRotateOnePlace {
     public static void main(String[] args) {
-       int[] arr = {1,2,3,4,5};
-        int timesOfRotate=5;
-        int backCounter=arr.length-1;
-        for (int i = 0; i <timesOfRotate% arr.length ; i++) {
-            int temp=arr[backCounter];
-            arr[backCounter]=arr[i];
-            arr[i]=temp;
-            backCounter--;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int d = 3;
+        int temp[] = new int[d];
+       //fill temp arr
+        for (int i = 0; i <d ; i++) {
+            temp[i]=arr[i];
+        }
+
+        //fill first upto d
+        for (int i = 0; i <=d ; i++) {
+            arr[i]=arr[d+i];
+        }
+
+        //rest part of array
+        int c=0;
+        for (int i = d+1; i <arr.length ; i++) {
+            arr[i]=temp[c++];
         }
         System.out.println(Arrays.toString(arr));
     }
