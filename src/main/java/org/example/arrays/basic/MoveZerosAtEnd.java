2a0681b45076efe6beb1d212605edd3543f01f61
ranjeetrathour <ranjeetrathour@example.com>
2026-04-10 17:58:39 +0100

    move zeros at end with one loop

--- src/main/java/org/example/arrays/basic/MoveZerosAtEnd.java
+++ src/main/java/org/example/arrays/basic/MoveZerosAtEnd.java
@@ -7,14 +7,27 @@ public class MoveZerosAtEnd {
         int[] arr = {1,2,0,7,0,3,0,1};
 
         int count=0;
-        for (int i=0; i< arr.length; i++){
+
+        for (int i = 0; i < arr.length ; i++) {
             if (arr[i]!=0){
-                arr[count++]=arr[i];
+                int temp=arr[i];
+                arr[i]=arr[count];
+                arr[count]=temp;
+                count++;
             }
         }
-        for (int i=count;i< arr.length; i++){
-            arr[count++]=0;
-        }
+
         System.out.println(Arrays.toString(arr));
     }
 }
+
+/** with two loop
+ *    for (int i=0; i< arr.length; i++){
+ *             if (arr[i]!=0){
+ *                 arr[count++]=arr[i];
+ *             }
+ *         }
+ *         for (int i=count;i< arr.length; i++){
+ *             arr[count++]=0;
+ *         }
+ */
