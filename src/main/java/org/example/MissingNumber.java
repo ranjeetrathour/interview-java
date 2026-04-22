88c57697698baf914afad6212f5fe06c60024043
ranjeetrathour <ranjeetrathour@example.com>
2026-04-22 08:25:33 +0100

    add missing number for unsorted array

--- src/main/java/org/example/MissingNumber.java
+++ src/main/java/org/example/MissingNumber.java
@@ -3,10 +3,24 @@ package org.example;
 public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {1,2,4,5};
-        for (int i = 0; i <arr.length-1 ; i++) {
-            if (arr[i]!=arr[i+1]-1){
-                System.out.println(arr[i]+1);
+        int[] hash = new int[arr.length+2];
+
+        for (int i = 0; i <arr.length ; i++) {
+            hash[arr[i]]=1;
+        }
+        for (int i = 1; i < hash.length ; i++) {
+            if (hash[i]==0){
+                System.out.println(i);
             }
         }
     }
 }
+
+/** this approach will work for sorted array
+ *  int[] arr = {1,2,4,5};
+ *         for (int i = 0; i <arr.length-1 ; i++) {
+ *             if (arr[i]!=arr[i+1]-1){
+ *                 System.out.println(arr[i]+1);
+ *             }
+ *         }
+ */
