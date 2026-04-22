8e21757f6b4d5bb7361a2f83fef29590da221340
ranjeetrathour <ranjeetrathour@example.com>
2026-04-22 08:29:44 +0100

    add missing number java 8 and formula

--- src/main/java/org/example/MissingNumber.java
+++ src/main/java/org/example/MissingNumber.java
@@ -1,18 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {1,2,4,5};
-        int[] hash = new int[arr.length+2];
 
-        for (int i = 0; i <arr.length ; i++) {
-            hash[arr[i]]=1;
-        }
-        for (int i = 1; i < hash.length ; i++) {
-            if (hash[i]==0){
-                System.out.println(i);
-            }
-        }
+        var result = (((arr.length+1)*(arr.length+2))/2)-(Arrays.stream(arr).sum());
+        System.out.println(result);
     }
 }
 
@@ -21,6 +16,21 @@ public class MissingNumber {
  *         for (int i = 0; i <arr.length-1 ; i++) {
  *             if (arr[i]!=arr[i+1]-1){
  *                 System.out.println(arr[i]+1);
+ *             }
+ *         }
+ */
+
+
+/**  for unsorted array
+ *      int[] arr = {1,2,4,5};
+ *         int[] hash = new int[arr.length+2];
+ *
+ *         for (int i = 0; i <arr.length ; i++) {
+ *             hash[arr[i]]=1;
+ *         }
+ *         for (int i = 1; i < hash.length ; i++) {
+ *             if (hash[i]==0){
+ *                 System.out.println(i);
  *             }
  *         }
  */
