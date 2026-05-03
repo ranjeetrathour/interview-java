a1a75b55314bd915793b23cb2b1c65a60f53579d
ranjeetrathour <ranjeetrathour@example.com>
2026-05-03 11:25:45 +0100

    longest sub array using prefixSum

--- src/main/java/org/example/arrays/basic/LongestSubArray.java
+++ src/main/java/org/example/arrays/basic/LongestSubArray.java
@@ -6,24 +6,43 @@ import java.util.OptionalInt;
 
 public class LongestSubArray {
     public static void main(String[] args) {
-        int[] array = {1,2,3,1,1,1,1,1,2,3,4};
+        int[] array = {1, 2, 3, 1, 1, 1, 1, 1, 2, 3, 4};
         int k = 3;
 
-        List<List<Integer>> list = new ArrayList<>();
+        int preSum =0; int maxLeng=0; int left=0;
 
-        for (int i = 0; i < array.length; i++) {
-            List<Integer> temp = new ArrayList<>();
-            for (int j = i; j < array.length; j++) {
-                temp.add(array[j]);
-                list.add(new ArrayList<>(temp));
+        for (int right = 0; right < array.length; right++) {
+            preSum=array[right]+preSum;
+
+            while (preSum>k && left<=right){
+                preSum-=array[left];
+                left++;
+            }
+            if (preSum==k){
+                maxLeng=Math.max(maxLeng,(right-left+1));
             }
         }
+        System.out.println(maxLeng);
 
-        OptionalInt max = list.stream()
-                .filter(integers -> integers.stream().mapToInt(Integer::intValue).sum() == k)
-                .toList()
-                .stream()
-                .map(List::size).mapToInt(Integer::intValue).max();
-        System.out.println(max);
     }
 }
+
+/**
+ *   List<List<Integer>> list = new ArrayList<>();
+ *
+ *         for (int i = 0; i < array.length; i++) {
+ *             List<Integer> temp = new ArrayList<>();
+ *             for (int j = i; j < array.length; j++) {
+ *                 temp.add(array[j]);
+ *                 list.add(new ArrayList<>(temp));
+ *             }
+ *         }
+ *
+ *         OptionalInt max = list.stream()
+ *                 .filter(integers -> integers.stream().mapToInt(Integer::intValue).sum() == k)
+ *                 .toList()
+ *                 .stream()
+ *                 .map(List::size).mapToInt(Integer::intValue).max();
+ *         System.out.println(max);
+ *     }
+ */
