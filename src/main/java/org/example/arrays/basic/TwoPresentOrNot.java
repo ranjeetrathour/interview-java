f5891d18de41afe77c2b9794aefbf92d8c5970db
ranjeetrathour <ranjeetrathour@example.com>
2026-05-06 17:32:32 +0100

    2 sum better

--- src/main/java/org/example/arrays/basic/TwoPresentOrNot.java
+++ src/main/java/org/example/arrays/basic/TwoPresentOrNot.java
@@ -7,8 +7,18 @@ public class TwoPresentOrNot {
     public static void main(String[] args) {
         int arr[]={2,6,5,8,11};
         int t=14;
+        int left=0; int right= arr.length-1;
 
-
+        while (left<right){
+            if (arr[left]+arr[right]==t){
+                System.out.println(true+" "+left+" "+right);
+                break;
+            } else if (arr[left]+arr[right]>t) {
+                right--;
+            }else{
+                left++;
+            }
+        }
     }
 }
 
@@ -23,7 +33,7 @@ public class TwoPresentOrNot {
  *         }
  */
 
-/** optimal
+/** better
  *
  *  Map<Integer,Integer> map = new HashMap<>();
  *
