da72ba3578860f964199a1133c0385f7b67aac29
ranjeetrathour <ranjeetrathour@example.com>
2026-05-18 17:46:17 +0100

    majority n/2 morve voting algo

--- src/main/java/org/example/arrays/basic/MajorityNBy2.java
+++ src/main/java/org/example/arrays/basic/MajorityNBy2.java
@@ -5,16 +5,27 @@ import java.util.Arrays;
 public class MajorityNBy2 {
     public static void main(String[] args) {
         int[] arr = {2, 2, 4, 4, 2, 4, 1, 4, 4, 4, 4, 4, 2, 4, 4, 4, 2};
-        int[] res = new int[9];
+        int cnt=0;
+        int ele=0;
 
-        for (int i = 0; i < arr.length; i++) {
-            res[arr[i]]=res[arr[i]]+1;
-        }
-
-        for (int i = 0; i < res.length; i++) {
-            if (res[i]>arr.length/2){
-                System.out.println(i);
+        for (int j : arr) {
+            if (cnt == 0) {
+                cnt++;
+                ele = j;
+            } else if (ele == j) {
+                cnt++;
+            } else {
+                cnt--;
             }
+        }
+        int cnt1=0;
+        for (int j : arr) {
+            if (j == ele) {
+                cnt1++;
+            }
+        }
+        if (cnt1>arr.length/2){
+            System.out.println(ele);
         }
     }
 }
@@ -34,4 +45,19 @@ public class MajorityNBy2 {
  * }
  * }
  * System.out.println(realValue);
+ */
+
+/**
+ * better
+ * int[] res = new int[9];
+ *
+ *         for (int i = 0; i < arr.length; i++) {
+ *             res[arr[i]]=res[arr[i]]+1;
+ *         }
+ *
+ *         for (int i = 0; i < res.length-1; i++) {
+ *             if (res[i]>arr.length/2){
+ *                 System.out.println(i);
+ *             }
+ *         }
  */
