48a19335be8e91a4f5b81a9e2b75944047954aad
ranjeetrathour <ranjeetrathour@example.com>
2026-05-18 17:33:58 +0100

    majority n/2 better

--- src/main/java/org/example/arrays/basic/MajorityNBy2.java
+++ src/main/java/org/example/arrays/basic/MajorityNBy2.java
@@ -1,22 +1,37 @@
 package org.example.arrays.basic;
 
+import java.util.Arrays;
+
 public class MajorityNBy2 {
     public static void main(String[] args) {
-        int []arr = {2,2,4,4,2,4,1,4,4,4,4,4,2,4,4,4,2};
-        System.out.println(arr.length);
-        int realValue = -1;
+        int[] arr = {2, 2, 4, 4, 2, 4, 1, 4, 4, 4, 4, 4, 2, 4, 4, 4, 2};
+        int[] res = new int[9];
+
         for (int i = 0; i < arr.length; i++) {
-            int counter =0;
-            for (int j = i+1; j <arr.length ; j++) {
-                if(arr[i]==arr[j]){
-                    counter++;
-                }
-            }
-            if(arr.length/2<=counter){
-               realValue=arr[i];
+            res[arr[i]]=res[arr[i]]+1;
+        }
+
+        for (int i = 0; i < res.length; i++) {
+            if (res[i]>arr.length/2){
+                System.out.println(i);
             }
         }
-        System.out.println(realValue);
-
     }
 }
+
+/**
+ * System.out.println(arr.length);
+ * int realValue = -1;
+ * for (int i = 0; i < arr.length; i++) {
+ * int counter =0;
+ * for (int j = i+1; j <arr.length ; j++) {
+ * if(arr[i]==arr[j]){
+ * counter++;
+ * }
+ * }
+ * if(arr.length/2<=counter){
+ * realValue=arr[i];
+ * }
+ * }
+ * System.out.println(realValue);
+ */
