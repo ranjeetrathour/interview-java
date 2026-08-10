46fd23b1c23d503e62db5e383a45c93137e0c3be
ranjeetrathour <ranjeetrathour@example.com>
2026-08-10 09:11:35 +0100

    compress string using java 8

--- src/main/java/org/example/streams/UniqueEvenNumber.java
+++ src/main/java/org/example/streams/UniqueEvenNumber.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 public class UniqueEvenNumber {
     public static void main(String[] args) {
         List<Integer> list = List.of(10,7,10,22,5,33,30,22,9,8);
-
         list.stream()
                 .filter(integer -> integer % 2 == 0)
                 .collect(Collectors.groupingBy(integer -> integer, Collectors.counting()))
